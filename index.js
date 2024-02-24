6ec0f26030344d9f580ec06c144c91e1074158e5
iamtun <letuan19431791.iuh@gmail.com>
2024-02-24 16:17:53 +0700

    create express app

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-﻿console.log('ha')
+﻿import express from 'express';
+
+const PORT = 3000;
+const app = express();
+
+//register middlewares
+app.use(express.json({extended: false}));
+app.use(express.static('./views'));
+
+//config view
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+// routers
+app.get('/', (req, resp) => resp.render('index'))
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
